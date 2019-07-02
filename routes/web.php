deb2f6d57a18c1bac63f174ad81177444817fb55
Dmitrii Smolkin <smolkin.do@gmail.com>
2019-07-02 12:28:27 +0300

    Initial authentication

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes(['verify' => true, 'register' => false]);
+
+Route::get('/management', 'ManagementController@dashboard')->name('dashboard');
